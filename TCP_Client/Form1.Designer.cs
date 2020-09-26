8903fa93fd1ad937767720613f5b6e1829929082
BeomJune <zxo9988@naver.com>
2020-09-26 17:58:53 +0900

    TCPClient연습

---
+++ TCP_Client/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace TCP_Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_ip = new System.Windows.Forms.TextBox();
+            this.tb_port = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_receive = new System.Windows.Forms.TextBox();
+            this.tb_writeline = new System.Windows.Forms.TextBox();
+            this.tb_connect = new System.Windows.Forms.Button();
+            this.bt_send = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_ID = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tb_ip
+            // 
+            this.tb_ip.Location = new System.Drawing.Point(309, 12);
+            this.tb_ip.Name = "tb_ip";
+            this.tb_ip.Size = new System.Drawing.Size(100, 21);
+            this.tb_ip.TabIndex = 0;
+            this.tb_ip.Text = "127.0.0.1";
+            // 
+            // tb_port
+            // 
+            this.tb_port.Location = new System.Drawing.Point(309, 39);
+            this.tb_port.Name = "tb_port";
+            this.tb_port.Size = new System.Drawing.Size(100, 21);
+            this.tb_port.TabIndex = 0;
+            this.tb_port.Text = "5000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 9F);
+            this.label1.Location = new System.Drawing.Point(258, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 9F);
+            this.label2.Location = new System.Drawing.Point(258, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "PORT :";
+            // 
+            // tb_receive
+            // 
+            this.tb_receive.Location = new System.Drawing.Point(12, 12);
+            this.tb_receive.Multiline = true;
+            this.tb_receive.Name = "tb_receive";
+            this.tb_receive.ReadOnly = true;
+            this.tb_receive.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tb_receive.Size = new System.Drawing.Size(242, 235);
+            this.tb_receive.TabIndex = 2;
+            // 
+            // tb_writeline
+            // 
+            this.tb_writeline.Location = new System.Drawing.Point(12, 253);
+            this.tb_writeline.Name = "tb_writeline";
+            this.tb_writeline.Size = new System.Drawing.Size(242, 21);
+            this.tb_writeline.TabIndex = 0;
+            // 
+            // tb_connect
+            // 
+            this.tb_connect.Location = new System.Drawing.Point(260, 93);
+            this.tb_connect.Name = "tb_connect";
+            this.tb_connect.Size = new System.Drawing.Size(149, 60);
+            this.tb_connect.TabIndex = 3;
+            this.tb_connect.Text = "Access";
+            this.tb_connect.UseVisualStyleBackColor = true;
+            this.tb_connect.Click += new System.EventHandler(this.tb_connect_Click);
+            // 
+            // bt_send
+            // 
+            this.bt_send.Enabled = false;
+            this.bt_send.Location = new System.Drawing.Point(261, 251);
+            this.bt_send.Name = "bt_send";
+            this.bt_send.Size = new System.Drawing.Size(75, 23);
+            this.bt_send.TabIndex = 3;
+            this.bt_send.Text = "Enter";
+            this.bt_send.UseVisualStyleBackColor = true;
+            this.bt_send.Click += new System.EventHandler(this.bt_send_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 9F);
+            this.label3.Location = new System.Drawing.Point(259, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ID :";
+            // 
+            // tb_ID
+            // 
+            this.tb_ID.Location = new System.Drawing.Point(309, 66);
+            this.tb_ID.Name = "tb_ID";
+            this.tb_ID.Size = new System.Drawing.Size(100, 21);
+            this.tb_ID.TabIndex = 5;
+            this.tb_ID.Text = "Client";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.bt_send;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 286);
+            this.Controls.Add(this.tb_ID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.bt_send);
+            this.Controls.Add(this.tb_connect);
+            this.Controls.Add(this.tb_receive);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_writeline);
+            this.Controls.Add(this.tb_port);
+            this.Controls.Add(this.tb_ip);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_ip;
+        private System.Windows.Forms.TextBox tb_port;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_receive;
+        private System.Windows.Forms.TextBox tb_writeline;
+        private System.Windows.Forms.Button tb_connect;
+        private System.Windows.Forms.Button bt_send;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_ID;
+    }
+}
+
---
+++ TCP_Client/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+namespace TCP_Client
+{
+    public partial class Form1 : Form
+    {
+
+        // 각종 전역 변수 선언
+        NetworkStream ntstream;
+        StreamReader streader;
+        StreamWriter stwriter;
+        Thread thread;
+        TcpClient client;
+        bool cflag = false;                                                          //Connect Flag
+        delegate void Dreceive(string data);                                         //델리게이트 (대리인) : 콜백 메소드 할 때 좋음
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Access Botton 클릭시 실행
+        private void tb_connect_Click(object sender, EventArgs e)
+        {
+            try
+            {   
+                if (!cflag)                                                        //현재 접속중인지 아닌지 판단.(첫 접속일 경우 전역변수 선언부에서 false로 선언하였기때문에 접속중이 아님) 
+                {
+                    client = new TcpClient(tb_ip.Text, int.Parse(tb_port.Text));   //텍스트 박스 값으로 TcpClient 생성 (int.parse 는 텍스트를 숫자화 하는 메서드)
+                    cflag = true;                                                 //접속하였기 때문에 접속 플래그를 True로 변경
+                    ntstream = client.GetStream();                                //접속한 Cilent에서 networkstream을 추출
+                    streader = new StreamReader(ntstream);                        //추출한 networkstream으로 streamreader,writer 생성
+                    stwriter = new StreamWriter(ntstream);
+                    client.ReceiveTimeout = 500;                                  //클라이언트의 ReceiveTimeout설정
+                    thread = new Thread(receive);                                 //receive메서드 스레드로 생성
+                    thread.Start();                                               //스레드 시작
+                    tb_recevie_text("접속이 성공하였습니다.\r\n");
+                    bt_send.Enabled = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                cflag = false;                                                    //실패할경우 접속이 취소되었음으로 플래그를 false로 변경
+                MessageBox.Show("접속에 실패하였습니다");
+            }
+        }
+        void receive()  
+        {
+            try
+            {
+                while (cflag)                                                     //현재 접속중인지 아닌지 판단
+                {
+                    string receive_data = streader.ReadLine();                    //streamreader의 한줄을 읽어들여 string 변수에 저장
+                    if (receive_data != null)                                     //data가 null이 아니면
+                    {
+                        tb_recevie_text(receive_data);                            //텍스트박스에 텍스트를 추가하는 메서드
+                    }
+                }
+            }
+            catch (IOException)                                                   //IO에러 (Timeout에러도 이쪽으로 걸림)
+            {
+                if (cflag)                                                        //현재 접속중인지 아닌지 체크
+                {
+                    thread = new Thread(receive);                                 //접속중일 경우 receive메서드를 이용한 스레드 다시생성
+                    thread.Start();
+                }
+                else
+                {
+                    cflag = false;                                                //접속중이 아닐경우 자연스럽게 스레드 정지
+                }
+            }
+            catch (Exception ex)                                                  //그 밖의 에러
+            {
+                cflag = false;                                                    //접속이 취소되었음으로 플래그를 false로 변경  
+                MessageBox.Show(ex.ToString());                                   //오류 내용 보고
+            }
+        }
+        void tb_recevie_text(string data)                                         //텍스트박스에 텍스트 추가하는 메서드
+        {
+            if (this.InvokeRequired)
+            {
+                this.Invoke(new Dreceive(tb_recevie_text),data);                  //델리게이트 관련 코드
+            }
+            else
+            {
+                if (data != null)                                                 //data가 null이 아닐경우
+                {
+                    tb_receive.AppendText(data + "\r\n");                         //텍스트박스에 데이터+개행문자 추가
+                }
+            }
+        }
+        private void bt_send_Click(object sender, EventArgs e)                    //전송 버튼
+        {
+            try
+            {
+                if (cflag)                                                        //현재 접속중인지 아닌지 체크
+                {
+                    if (ntstream.CanRead)
+                    {
+                        if (tb_writeline.Text != string.Empty)                   //전송할 내용이 담긴 TextBox가 비었는지 체크
+                        {
+                            tb_receive.AppendText(tb_ID.Text + " : " + tb_writeline.Text + "\r\n");
+                            stwriter.WriteLine(tb_ID.Text + " : " + tb_writeline.Text);                  //StreamWriter 버퍼에 텍스트박스 내용 저장
+                            stwriter.Flush();                                                            //StreamWriter 버퍼 내용을 스트림으로 전달
+                            tb_writeline.Text = null;
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)                                                   //에러
+            {
+                cflag = false;                                                    //접속이 취소되었음으로 플래그를 false로 변경
+                MessageBox.Show(ex.ToString());                                   //에러 내용 보고
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)     //FormClosing 이벤트x
+        {
+            if (cflag)
+           {
+	          if (ntstream.CanRead )
+	            {
+	                stwriter.WriteLine(tb_ID.Text + "의 연결이 끊어졌습니다. ");  //StreamWriter 버퍼에 텍스트박스 내용 저장
+	                stwriter.Flush();
+	                ntstream.Close();
+	            }
+                cflag = false;                                                    //접속중인지 체크하는 _connect_flag를 false로 변경
+            }
+        }
+    }
+}
